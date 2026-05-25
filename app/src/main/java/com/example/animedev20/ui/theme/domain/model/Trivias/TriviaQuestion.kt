de5e913806459aa1270a0a5e52e6749679a7cc70
Daniel Cuestas <102336853+dancuest@users.noreply.github.com>
2026-05-25 11:59:18 -0500

    mejoras a modulo principal de trivias

--- app/src/main/java/com/example/animedev20/ui/theme/domain/model/Trivias/TriviaQuestion.kt
+++ app/src/main/java/com/example/animedev20/ui/theme/domain/model/Trivias/TriviaQuestion.kt
@@ -3,11 +3,11 @@ package com.example.animedev20.ui.theme.domain.model.Trivias
 
 
 data class TriviaQuestion(
-                          val id: String,
-                          val animeId: Long,
-                          val difficulty: TriviaDifficulty,
-                          val question: String,
-                          val options: List<String>,
-                          val correctAnswerIndex: Int,
-                          val feedback: String
+    val id: String,
+    val animeId: Long,
+    val difficulty: TriviaDifficulty,
+    val question: String,
+    val options: List<String>,
+    val correctAnswerIndex: Int,
+    val feedback: String
 )
